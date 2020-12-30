96f3ab82aad69cacad0c718eb9dd9a6a962e3a5a
Arthik <arthikrb@gmail.com>
2020-12-30 13:17:34 +0530

    Added items

--- src/Items.java
+++ src/Items.java
@@ -10,7 +10,31 @@ public class Items
             {"105", "Surf Excel", "45"},
             {"106", "Bulb", "120"},
             {"107", "Bag", "750"},
-            {"108", "Shirt", "499"}
+            {"108", "Shirt", "499"},
+            {"109","Toothpaste","50"},
+            {"110","Water Bottle","200"},
+            {"111","Shoes","549"},
+            {"112","Notebook","30"},
+            {"113","Toothbrush","20"},
+            {"114","Oil","65"},
+            {"115","Masks","40"},
+            {"116","Soap","32"},
+            {"117","Brooms","149"},
+            {"118","Blankets","249"},
+            {"119","Vicks Vaporub","32"}, // ( ͡° ͜ʖ ͡°)
+            {"120","Pen","5"},
+            {"121","Chocolates","50"},
+            {"122","Buckets","150"},
+            {"123","Locks","45"},
+            {"124","Rice","86"},
+            {"125","Wheat","92"},
+            {"126","Batteries","13"},
+            {"127","Belts","120"},
+            {"128","Pen Case","80"},
+            {"129","Towels","120"},
+            {"130","Powder","60"},
+
+
     };
 
     public void display()throws IOException
